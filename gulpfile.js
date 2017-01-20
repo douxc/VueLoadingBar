18d1b726af8add1c629ca7a81e68beb5261e358f
douxc <dxinchao@gmail.com>
2017-01-20 17:06:06 +0800

    重新构建

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,14 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
 var cleanCss = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
 gulp.task('css', function() {
     gulp.src('./src/*.css')
         .pipe(autoprefixer({ browsers: ['Android >= 4.2', 'iOS >= 9', '> 5%'] }))
         .pipe(cleanCss())
-        .pipe(rename('loadingbar.min.css'))
-        .pipe(gulp.dest('./src'));
+        .pipe(gulp.dest('./dist'));
 });
+gulp.task('js', function() {
+    gulp.src('./src/*.js').pipe(uglify()).pipe(gulp.dest('./dist'));
+});
+gulp.task('dist', ['js', 'css']);
